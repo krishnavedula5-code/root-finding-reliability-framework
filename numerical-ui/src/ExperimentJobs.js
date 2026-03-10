7cc5f9cef689186070c0f29bbc4503e41e3cee1c
Krishna Vedula <krishnavedula5@gmail.com>
2026-03-10 10:16:49 +0530

    Centralize update frontend API URL

--- numerical-ui/src/ExperimentJobs.js
+++ numerical-ui/src/ExperimentJobs.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const API =
-  process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
+import { API } from "./api";
 
 export default function ExperimentJobs() {
   const [jobs, setJobs] = useState([]);
--- numerical-ui/src/ExperimentsDashboard.js
+++ numerical-ui/src/ExperimentsDashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
-const API =  process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
+import { API } from "./api";
 
 
 const METHOD_OPTIONS = [
